db6cbd49561ebac43340d0913c3ca14ec410b016
Paolo Monella <paolo.monella@gmx.net>
2017-03-15 19:45:07 +0100

    Reviewed (for the 2nd time) transcription of folio 1r

--- minidomToolBox/paoloMinidom.py
+++ minidomToolBox/paoloMinidom.py
@@ -1,12 +1,21 @@
 # !/usr/bin/env python
-# This is a toolbox I'm using to look for specific thinks in the XML DOM
+# This is a toolbox I'm using to look for specific things in the XML DOM
 from xml.dom.minidom import parse, parseString
 #import xml.dom.minidom
 xmldoc = parse('/home/ilbuonme/siti/paolo.monella/ursus/casanatensis.xml')
 wordElementList = xmldoc.getElementsByTagName('ref')
+x = False
 for r in wordElementList:
-    print('cRef: '+r.attributes.getNamedItem('cRef').nodeValue)
+    #print('cRef: '+r.attributes.getNamedItem('cRef').nodeValue)
     for c in r.childNodes:
         if c.nodeType == c.ELEMENT_NODE:
-            if (c.tagName == 'ref'):
-                print('Eureka!')
+            if c.tagName == 'w' and x:
+                print(c.attributes.getNamedItem('xml:id').nodeValue, end=' è la parola che viene dopo ')
+                x = False
+            if c.tagName == 'unclear':
+                for w in c.childNodes:
+                    #print(x, end=', ')
+                    if w.nodeType == w.ELEMENT_NODE and w.tagName == 'pc':
+                        print('Eureka!')
+                        print(w.attributes.getNamedItem('n').nodeValue)
+                        x = True
