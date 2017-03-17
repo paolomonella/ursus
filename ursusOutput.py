8a2db55155b15d47a02a2ec3d901a0c0977b538b
Paolo Monella <paolo.monella@gmx.net>
2017-03-17 20:51:42 +0100

    Treatment of pc, lb, cb, pb, note

--- ursusOutput.py
+++ ursusOutput.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# This script turns my own 'paolomarkdown' conventions into XML/TEI for the digital edition
-# of manuscript Casanatensis 1086.
+# This script simplifies the encoding of casanatensis.xml
+# and creates a file casanatensis_AL.xml with the Alphabetic
+# Layer only. 
 # It's written in Python 3.4, but also works with Python 2.7
-#
-# Warning: when writing in 'paolomarkdown', note that every string at the beginning
-# of the line that does not start with '<' or end with '>'
-# will be substituted by this script with
-# <w ana="string">string</w>
-# So, if this is not intended, every alphanumerical string that must not be substituted
-# should start with whitespace, such as tab or space(s)
+# To-do: continue checking elements and dealing with them
+#   - <pc> has annoying space before
+#   - <lb> also leaves a blank space
 
 from __future__ import print_function
 import datetime
@@ -21,83 +18,95 @@ import re
 import xml.etree.ElementTree as ET
 
 # Clear screen
-#os.system('clear')
+os.system('clear')
 
-# The namespaces
-t    = '{http://www.tei-c.org/ns/1.0}'              # for XML/TEI
-xml   = '{http://www.w3.org/XML/1998/namespace}'   # for attributes like xml:id
-#ET.register_namespace('', 'http://www.tei-c.org/ns/1.0')
+# Namespaces
+n = '{http://www.tei-c.org/ns/1.0}'              # for XML/TEI
+xml = '{http://www.w3.org/XML/1998/namespace}'   # for attributes like xml:id
+ET.register_namespace('', 'http://www.tei-c.org/ns/1.0')
 ns = {'tei': 'http://www.tei-c.org/ns/1.0',             # for TEI XML
         'xml': 'http://www.w3.org/XML/1998/namespace'}  # for attributes like xml:id
 
-# Parse the tree
-tree = ET.parse('casanatensis.xml')
-#root = tree.getroot()
+# Parse the tree of casanatensis.xml
+casanaTree = ET.parse('casanatensis.xml')
+# Parse the tree of the ALIM2 template: it will be the base for the output tree
+tree = ET.parse('ALIM2_publication/teiHeader_template.xml')
+root = tree.getroot()
+root.set('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance')
+
+# Append the <body> of casanatensis.xml into <text> of the output xml file
+myBody = casanaTree.getroot().find(n + 'text').find(n + 'body')
+myText = root.find(n + 'text')        # <text> of output xml file
+myText.append(myBody)
+
+#############
+# Functions #
+#############
+
+def manageWordLikeElem(w):
+    pass
 
 
-def manageWordLikeElem(elem):
-    ow.append(elem.tag)
+
+#######################
+# Traverse the <body> # 
+#######################
 
 #for e in tree.findall('.//' + '*', ns):
-ow = [] # Other word-like elements
-for ab in tree.findall('.//' + t + 'ab'):
-    # § Replicate in output xml
+for ab in tree.findall('.//' + n + 'ab'):
+    # Insert a <head> with the title of the section
+    if ab.get('n'):
+        newHead = ET.Element('head')
+        newHead.text = ab.get('n')
+        ab.insert(0, newHead)
     for ref in ab: # Iterate over the <ref> children of <ab>
-        for w in ref: # Iterate over word-like elements (such as <w>, <gap>, <pc> etc.)
-            manageWordLikeElem(w)
+        for wLike in ref: # Iterate over word-like elements (such as <w>, <gap>, <pc> etc.)
+            manageWordLikeElem(wLike)
+
+#######################################
+# Substitute/delete specific elements #
+#######################################
+
+# Turn <lb>, <cb> and <pb> into anchors
+for x in ['lb', 'cb', 'pb']:    
+    for br in tree.findall('.//' + n + x):
+        br.tag = 'anchor'
+
+# Delete all elements with name 'myElemName' and namespace myNameSpace. 
+def deleteAllElements(myElemName, myNameSpace):
+    search = ('.//{0}' + myElemName).format(myNameSpace)
+    my_elem_parents = tree.findall(search + '/..')
+    for x in my_elem_parents:
+        for y in x.findall(myNameSpace + myElemName):
+            x.remove(y)
+            #print(y.tag)
+
+deleteAllElements('note', n)
+
+for pc in tree.findall('.//' + n + 'pc'):
+    if pc.get('n') != '0':
+        pc.text = pc.get('n').replace('question', '?').replace('quote', '"').replace('space', ' ')
+
+"""
+for x in ['note']: # I'm making this a list, just in case I want to get rid of more elements
+    for br in tree.findall('.//' + n + x):
+        #print(br.find('..'))
+        print(.//{0}prop.format(n))
+        """
 
 """
 This is the list of word-like elements, possible children of <ref>:
-{http://www.tei-c.org/ns/1.0}lb     # Replicate
-{http://www.tei-c.org/ns/1.0}cb     # Replicate
-{http://www.tei-c.org/ns/1.0}pb     # Replicate
-{http://www.tei-c.org/ns/1.0}pc     # Use @n
-{http://www.tei-c.org/ns/1.0}note   # Replicate?
+{http://www.tei-c.org/ns/1.0}lb     # Turn into <anchor>... or just delete
+{http://www.tei-c.org/ns/1.0}cb     # same as above, but possibly anchor
+{http://www.tei-c.org/ns/1.0}pb     # same as above, but possibly anchor
+{http://www.tei-c.org/ns/1.0}pc     # Use @n as text content
+{http://www.tei-c.org/ns/1.0}note   # Replicate? Nope, delete
 {http://www.tei-c.org/ns/1.0}milestone  # Replicate?
-{http://www.tei-c.org/ns/1.0}w      # Replicate?
-{http://www.tei-c.org/ns/1.0}anchor # Ignore
+{http://www.tei-c.org/ns/1.0}w      # Replicate
+{http://www.tei-c.org/ns/1.0}anchor # Replicate
 {http://www.tei-c.org/ns/1.0}add
 {http://www.tei-c.org/ns/1.0}unclear
 {http://www.tei-c.org/ns/1.0}gap
 
 """
-for o in set(ow):
-    print(o)
-
-"""
-    if word.get(nx + 'id'):
-        idstring = word.get(nx + 'id')
-        idcount  = int(idstring[1:])
-    else:
-        idcount  = idcount + 3
-        idstring = 'w' + str(idcount)
-        word.set(nx + 'id', 'w' + str(idcount))
-
-# Quando risolvo il problema, devo de-commentare la riga seguente:
-tree.write('output.xml', encoding="UTF-8", method="xml")
-
-############################################
-# Check that all IDs are sequential so far #
-############################################
-
-# ... and if there are words without an ID still
-
-# Parse the tree
-tree = ET.parse('output.xml')
-
-idcount = 0
-for word in tree.findall('.//' + n + 'w'):
-    if word.get(nx + 'id'):
-        idstring = word.get(nx + 'id')
-        old_idcount  = idcount
-        idcount  = int(idstring[1:])
-        if idcount > old_idcount:
-            #print(str(idcount) + ' > ' + str(old_idcount))
-            pass
-        else:
-            print('Nuovo counter: ' + str(idcount) + '. Counter precedente: ' + str(old_idcount))
-
-
-
-
-"""
+tree.write('ALIM2_publication/casanatensis_AL.xml', encoding='UTF-8', method='xml')
